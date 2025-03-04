8d165788cbd07b2b5f9d635687dcc24e3eec4030
willymariino <wg.mariino@gmail.com>
2025-03-04 17:11:04 +0100

    lampElement .append

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const lampElement = document.querySelector(".white_lamp");
 console.log(lampElement)
+lampElement.addEventListener('click', function () {
+
+    lampElement.append(Image)
+
+
+}
+
+
+)
