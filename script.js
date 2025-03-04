56aa60fc51211f7c6a040adcd6c536dd39867b7f
willymariino <wg.mariino@gmail.com>
2025-03-04 17:24:09 +0100

    image search

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log(lampElement)
 lampElement.addEventListener('click', function () {
     console.log("ho cliccato sul div")
 
-    lampElement.innerHTML += Image
+    lampElement.innerHTML += "<img src="img / yellow_lamp.png" alt="">"
 
 
 }
