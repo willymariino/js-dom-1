9525d58645e7b816ae284605907e3cc0b3a3e3d5
willymariino <wg.mariino@gmail.com>
2025-03-04 17:12:01 +0100

    console.log

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const lampElement = document.querySelector(".white_lamp");
 console.log(lampElement)
 lampElement.addEventListener('click', function () {
+    console.log("ho cliccato sul div")
 
     lampElement.append(Image)
 
