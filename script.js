fd00f3dc9f7c46866122e8f51b66364bf73b3cdb
willymariino <wg.mariino@gmail.com>
2025-03-04 17:38:57 +0100

    lampelement.src

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ console.log(lampElement)
 lampElement.addEventListener('click', function () {
     console.log("ho cliccato sul div")
 
-    lampElement.innerHTML += <img src="img / yellow_lamp.png" alt="">
+    lampElement.src = 'img/yellow_lamp.png'
 })
