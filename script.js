85c8deb1cb35ba33701c5fbd3bd55773c6c821db
willymariino <wg.mariino@gmail.com>
2025-03-04 17:26:59 +0100

    error parentesis

--- script.js
+++ script.js
@@ -3,9 +3,5 @@ console.log(lampElement)
 lampElement.addEventListener('click', function () {
     console.log("ho cliccato sul div")
 
-    lampElement.innerHTML += "<img src="img / yellow_lamp.png" alt="">"
-
-
-}
-
-)
+    lampElement.innerHTML += <img src="img / yellow_lamp.png" alt="">
+})
